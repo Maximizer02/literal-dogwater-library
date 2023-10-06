83949deef7301109778e5e63f89fe03105a8b084
Maximizer02 <146656033+Maximizer02@users.noreply.github.com>
2023-10-07 01:12:43 +0200

    implemented isOdd() method

--- lib/src/main/java/literaldogwaterlibrary/Library.java
+++ lib/src/main/java/literaldogwaterlibrary/Library.java
@@ -3,8 +3,26 @@
  */
 package literaldogwaterlibrary;
 
-public class Library {
-    public boolean someLibraryMethod() {
+public class Library
+{
+    public boolean someLibraryMethod()
+    {
         return true;
+    }
+
+    public String test()
+    {
+        return "kekw";
+    }
+
+    public boolean isOdd(int number)
+    {
+        switch (number % 2)
+        {
+            case 0:
+                return false;
+            default:
+                return true;
+        }
     }
 }
